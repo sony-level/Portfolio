6de8a2375a547d0c32469949c38dfec75a4b8538
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-10-29 13:33:47 +0100

    add analityc models

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
   Projects,
   BlogPosts,
   Loading,
+  CVE,
   Achievements, Stats,
 } from "./components";
 
@@ -66,6 +67,7 @@ const App = () => {
             <OpenSource />
             <BlogPosts enabled={true} />
             <ExtraCurricular />
+
           </div>
         </div>
         <Footer />
